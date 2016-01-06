da1c3bc8ee94f9fbe6dd67a1c37940dc8b0157a0
jonpolbob@gmail.com <jonpolbob@gmail.com>
2016-01-06 14:19:06 +0100

    Signed-off-by: jonpolbob@gmail.com <jonpolbob@gmail.com>

---
+++ arduinofinder/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+using System.IO;
+
+
+namespace arduinofinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagementScope connectionScope = new ManagementScope();
+      
+            StreamWriter standardOutput = new StreamWriter(Console.OpenStandardOutput());
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);
+            Console.WriteLine("viewpoint device detector");
+
+            AutodetectArduinoPort();
+
+            Console.WriteLine("press any key to continue");
+
+            Console.ReadKey();
+
+          }
+            static private string AutodetectArduinoPort()
+        {
+            ManagementScope connectionScope = new ManagementScope();
+            SelectQuery serialQuery = new SelectQuery("SELECT * FROM Win32_SerialPort");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, serialQuery);
+
+            //StreamWriter standardOutput = new StreamWriter(Console.OpenStandardOutput());
+            //standardOutput.AutoFlush = true;
+            //Console.SetOut(standardOutput);
+      
+
+
+            try
+            {
+                foreach (ManagementObject item in searcher.Get())
+                {
+                    string desc = item["Description"].ToString();
+                    string deviceId = item["DeviceID"].ToString();
+
+                    if (desc.Contains("Arduino"))
+                    {
+                        Console.WriteLine("Arduino found on {0} ({1})]", deviceId, desc);
+                    }
+                }
+            }
+            catch (ManagementException e)
+            {
+                /* Do Nothing */
+            }
+
+            return null;
+        }
+        }
+    }
+
